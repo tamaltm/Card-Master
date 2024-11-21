f8ddea7f6d7c30545f795a9dbb3ae713c554c49f
Tamal Mazumder <tamaltm143@gmail.com>
2024-11-21 19:21:20 +0600

    hearts design

--- Card Master/src/HTable.java
+++ Card Master/src/HTable.java
@@ -1,4 +1,26 @@
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Color;
+public class HTable extends Pile{
+    public HTable(int x,int y){
+        super(x,y);
+        super.setSize(500,600);
+        super.setOpaque(false);
+    }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.white);
+        g2d.drawLine(0, 0, 72, 0);
+        g2d.drawLine(0,0,0,96);
+        g2d.drawLine(71,0,71,96);
 
-public class HTable {
-
+        int cardinc = 0;
+        if(!isEmpty()){
+            for(Card c :cards){
+                    g.drawImage(c.CardImage(), 0, cardinc, 72,96,this);
+                    cardinc+=30;
+            }
+        }
+    }
 }
--- Card Master/src/Hpanel.java
+++ Card Master/src/Hpanel.java
@@ -7,6 +7,30 @@ public class Hpanel extends JPanel{
     Hpanel(){
         setBackground(Color.GREEN);
         super.setLayout(null);
-        
+        deck = new Deck(ALLBITS, ABORT);
+        player1 = new HTable[13];
+        for(int i=0;i<player1.length;i++){
+            player1[i] = new HTable(460-i*30,395);
+            player1[i].push(deck.pop());
+            add(player1[i]);
+        }
+        player2 = new HTable[13];
+        for(int i=0;i<player2.length;i++){
+            player2[i] = new HTable(540,390-i*30);
+            player2[i].push(deck.pop());
+            add(player2[i]);
+        }
+        player3 = new HTable[13];
+        for(int i=0;i<player3.length;i++){
+            player3[i] = new HTable(460-i*30,30);
+            player3[i].push(deck.pop());
+            add(player3[i]);
+        }
+        player4 = new HTable[13];
+        for(int i=0;i<player4.length;i++){
+            player4[i] = new HTable(15,390-i*30);
+            player4[i].push(deck.pop());
+            add(player4[i]);
+        }
     }
 }
